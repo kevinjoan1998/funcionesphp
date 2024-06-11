948182d2670329f49cfde8d5e48d4b7706a3356f
kevinjoan1998 <122640128+kevinjoan1998@users.noreply.github.com>
2024-06-11 00:10:05 -0500

    ralizar cambios en el index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/*function get_pokemon() {
+function get_pokemon() {
     $numero_aleatorio = rand(1, 5);
     switch ($numero_aleatorio) {
         case 1:
@@ -22,7 +22,7 @@ for ($i = 0; $i < 20; $i++) {
 }
 ?>
 
-<?php*/
+<?php 
 function buscar_dia($dia_buscado) {
     // Convertimos a minúsculas
     $dia_buscado = strtolower($dia_buscado);
@@ -51,3 +51,42 @@ function buscar_dia($dia_buscado) {
 $dia_buscado = "Lunes";
 buscar_dia($dia_buscado);
 ?>
+
+
+
+
+
+<?php
+function sumar($a, $b){
+  // Se verifica si las variables $a y $b son numéricas
+  if (is_numeric($a) && is_numeric($b)) {
+    $resp = $a + $b;
+    echo "El resultado de la suma de $a + $b: $resp";
+  } else {
+    echo "Error: Las variables deben ser numéricas.";
+  }
+}
+
+// Se llama a la función sumar con dos argumentos
+sumar(4, 5); 
+
+?>
+
+
+<?php
+if (!function_exists('sumar')) {
+  function sumar($a = 0, $b = 0) {
+    $resp = $a + $b;
+    echo "El resultado de la suma de $a + $b: $resp";
+  }
+}
+
+$arreglos = [1, 2];
+
+// Usando el operador de propagación de argumentos
+sumar(...$arreglos);
+// Sin el operador de propagación
+sumar($arreglos[0], $arreglos[1]); // Se pasan los elementos del array como argumentos individuales
+
+// Con el operador de propagación
+sumar(...$arreglos); // Equivalente a sumar(1, 2)
